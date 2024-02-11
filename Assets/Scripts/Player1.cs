e24b4a59a2302402268dcd6ac50ad1584ac428f5
David Woodman <dav.woodman@gmail.com>
2024-02-11 17:38:52 -0500

    player movement and weird camera behavior added

--- Assets/Scripts/Player1.cs
+++ Assets/Scripts/Player1.cs
@@ -6,6 +6,7 @@ public class Player1 : MonoBehaviour
 {
 
     public GameObject playerPrefab;
+    public float moveSpeed = 5f; // Speed of player movement
 
     // Start is called before the first frame update
     void Start()
@@ -16,13 +17,21 @@ public class Player1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Get input from the player
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Calculate movement direction
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+
+        // Move the player
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
     }
 
     public void SpawnPlayer(Map map)
     {
         // Calculate the center position of the map
-        Vector3 centerPosition = new Vector3(map.mapWidth / 2 * map.tileSize, 0, map.mapHeight / 2 * map.tileSize);
+        Vector3 centerPosition = new Vector3(map.mapWidth / 2 * map.tileSize, map.tileSize/2, map.mapHeight / 2 * map.tileSize);
 
         // Instantiate the player prefab at the center position
         Instantiate(playerPrefab, centerPosition, Quaternion.identity);
